8836d09fe7b1fe38f886796fd4891c8814385c18
ymrl <ymrl@ymrl.net>
2018-03-13 01:04:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SKK-JISYO.emoji-ja
+
+SKK用のUTF-8絵文字辞書です。
+
+[Mozc プロジエクト](https://github.com/google/mozc)の [emoji_data.tsv](https://github.com/google/mozc/blob/master/src/data/emoji/emoji_data.tsv) から生成しています。
+
+すべての絵文字を網羅しているわけではないので、 [skk-emoji-jisyo](https://github.com/uasi/skk-emoji-jisyo) との併用をおすすめします。
---
+++ generate.rb
@@ -0,0 +1,31 @@
+#! /usr/bin/env ruby
+
+require 'open-uri'
+require 'nkf'
+
+data = open('https://raw.githubusercontent.com/google/mozc/master/src/data/emoji/emoji_data.tsv').read
+result = {}
+data.each_line do |line|
+    next if line =~ /^#.*/
+    arr = line.split(/\t/)
+    next if arr[0] == ''
+    arr[6].split(/\s/).each do |yomi|
+        regular_yomi = NKF.nkf('-Z0wW', yomi).downcase
+        result[regular_yomi] = [] unless result[regular_yomi]
+        result[regular_yomi].push arr[1]
+    end
+end
+
+
+open('SKK-JISYO.emoji-ja.utf8', 'w') do |f|
+    f << ";; skk-emoji-jisyo-ja\n"
+    open('LICENSE').each_line {|line| f << ";; #{line}" }
+    f << ";;\n"
+    f << ";;\n"
+    f << ";; Original emoji_data.tsv from Mozc Project\n"
+    f << ";; https://github.com/google/mozc\n"
+    f << ";;\n"
+    open('https://raw.githubusercontent.com/google/mozc/master/LICENSE').each_line {|line| f << ";; #{line}" }
+    f << ";;\n"
+    f << result.map{|k,v| "#{k} /#{v.join('/')}/"}.join("\n")
+end
